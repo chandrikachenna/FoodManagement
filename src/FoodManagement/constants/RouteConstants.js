32042ca6728beaa825d837c2d868c5766906953f
cHanDu-ri <chennachandrika@gmail.com>
2020-06-02 09:57:13 +0530

    routeSettings

--- src/FoodManagement/constants/RouteConstants.js
+++ src/FoodManagement/constants/RouteConstants.js
@@ -1,2 +1,2 @@
 export const SMART_FOOD_MANAGEMENT_PATH = "/food-management/home";
-export const MEAL_PREFERENCE_PATH=  '/food-management/set-meal-preference';
+export const MEAL_PREFERENCE_PATH=  '/food-management/set-meal-preference/';
