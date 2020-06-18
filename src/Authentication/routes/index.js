e584743cc08c163f8c99ee27ffe1c4aeaf881fd7
cHanDu <chennachandrika@gmail.com>
2020-06-18 11:42:44 +0530

    clear signupPage

--- src/Authentication/routes/index.js
+++ src/Authentication/routes/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { SIGN_UP_PATH, SIGN_IN_PATH } from '../constants/RouteConstants'
 
-import { SignupRoute } from './SignupRoute'
 import { SigninRoute } from './SigninRoute'
 
 const routes = [<Route exact path={SIGN_IN_PATH} component={SigninRoute} />]
