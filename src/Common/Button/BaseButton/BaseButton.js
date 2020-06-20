a882a429bf2346ba2d9fdb00e9595907df36bdf5
cHanDu <chennachandrika@gmail.com>
2020-06-20 15:02:29 +0530

    defaultProps for button

--- src/Common/Button/BaseButton/BaseButton.js
+++ src/Common/Button/BaseButton/BaseButton.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import {ButtonWrapper} from './styledComponents';
 
+
 class BaseButton extends Component {
-    //TODO:defaultProps textTypo
+    static defaultProps={
+        className:'',
+        TextTypo:span
+    }
     isEnabled=()=>{
         const {disabled}=this.props
         return !disabled
     }
     render() {
-        const {className,onClick,text,textTypo:TextType,buttonCss}=this.props;
+        const {className,onClick,text,textTypo:TextTypo,buttonCss}=this.props;
         let otherProps={}
         if(this.isEnabled()){
             otherProps.onClick=onClick
         }
         return (
             <ButtonWrapper css={buttonCss} className={className} {...otherProps}>
-                <TextType>{text}</TextType>
+                <TextTypo>{text}</TextTypo>
             </ButtonWrapper>
         )
     }
